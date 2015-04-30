974759c6a00c2314ecbac374d674a22800fd6e67
Brian Newton <notthemessiah63@gmail.com>
2015-04-30 17:24:21 +0100

    fixed the previousOwners

--- src/car.js
+++ src/car.js
@@ -1,6 +1,7 @@
-function Car(make, model, year, color, state){
+function Car(make, model, year, color, state, previousOwners){
   this.year = year;
   this.state = "off"
+  this.previousOwners = ""
 }
 
 Car.prototype.sale = function(newOwner){
